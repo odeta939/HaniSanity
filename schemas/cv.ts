b5a2bdd167fd5e54db153d1bb744696f521c281f
odeta939 <odetapenikaite@gmail.com>
2023-02-15 14:46:06 +0100

    initial commit

---
+++ schemas/cv.ts
@@ -0,0 +1,12 @@
+export default{
+    name: "CV",
+    type: "document",
+    title: "CV",
+    fields: [
+        {
+            name: "Name",
+            type: "string",
+            title: "Name"
+        }
+    ]
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,2 @@
-export const schemaTypes = []
+import cv from "./cv"
+export const schemaTypes = [cv]
